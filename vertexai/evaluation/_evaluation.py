889bdd5bf958496e89133a965b4b3e26b22cf3a6
sahusiddharth <siddharth.sahu@plaksha.edu.in>
2025-04-16 20:24:51 +0530

    base

--- vertexai/evaluation/_evaluation.py
+++ vertexai/evaluation/_evaluation.py
@@ -26,6 +26,11 @@ from google.cloud.aiplatform import base
 from google.cloud.aiplatform_v1beta1.types import (
     content as gapic_content_types,
 )
+
+from ragas.metrics.base import Metric as RagasMetric
+from ragas.dataset_schema import EvaluationDataset as RagasEvaluationDataset
+from ragas import evaluate as ragas_evaluate
+
 from vertexai import generative_models
 from vertexai.evaluation import _base as evaluation_base
 from vertexai.evaluation import constants
@@ -131,6 +136,7 @@ def _validate_dataset(
     _validate_response_column_required(evaluation_run_config)
     _validate_reference_column_required(evaluation_run_config)
     _validate_reference_or_source_column_required(evaluation_run_config)
+    _validate_ragas_metrics_columns(evaluation_run_config)
 
 
 def _validate_response_column_required(
@@ -223,9 +229,9 @@ def _compute_custom_metrics(
         for future in futures_list:
             metric_output = future.result()
             try:
-                row_dict[
-                    f"{custom_metric.name}/{constants.MetricResult.SCORE_KEY}"
-                ] = metric_output[custom_metric.name]
+                row_dict[f"{custom_metric.name}/{constants.MetricResult.SCORE_KEY}"] = (
+                    metric_output[custom_metric.name]
+                )
             except KeyError:
                 raise KeyError(
                     f"Custom metric score `{custom_metric.name}` not found in"
@@ -241,18 +247,55 @@ def _compute_custom_metrics(
     return row_dict
 
 
-def _separate_custom_metrics(
+def _ragas_metric_required_columns(
+    evaluation_run_config: evaluation_base.EvaluationRunConfig,
+):
+    """Returns list of required columns for ragas metrics."""
+    required_columns = set()
+    for metric in evaluation_run_config.metrics:
+        if isinstance(metric, RagasMetric) and isinstance(
+            metric.required_columns, dict
+        ):
+            required_columns.update(metric.required_columns.get("SINGLE_TURN"))
+    return list(required_columns)
+
+
+def _validate_ragas_metrics_columns(
+    evaluation_run_config: evaluation_base.EvaluationRunConfig,
+):
+    """Checks if all columns required for ragas metrics calculation exists in the dataset."""
+    required_columns = _ragas_metric_required_columns(evaluation_run_config)
+    for required in required_columns:
+        if required not in evaluation_run_config.dataset.columns:
+            raise KeyError(
+                "Required column"
+                f" `{required}`"
+                " not found in the evaluation dataset. The columns in the"
+                f" evaluation dataset are {list(evaluation_run_config.dataset.columns)}."
+            )
+    return RagasEvaluationDataset.from_pandas(
+        dataframe=evaluation_run_config.dataset[required_columns]
+    )
+
+
+def _separate_metrics(
     metrics: List[Union[str, metrics_base._Metric]],
-) -> Tuple[List[Union[str, metrics_base._Metric]], List[metrics_base.CustomMetric],]:
-    """Separates the metrics list into API and custom metrics."""
-    custom_metrics = []
+) -> Tuple[
+    List[Union[str, metrics_base._Metric]],
+    List[metrics_base.CustomMetric],
+]:
+    """Separates the metrics list into API, custom and Ragas metrics."""
     api_metrics = []
+    custom_metrics = []
+    ragas_metrics = []
     for metric in metrics:
         if isinstance(metric, metrics_base.CustomMetric):
             custom_metrics.append(metric)
+        elif isinstance(metric, RagasMetric):
+            ragas_metrics.append(metric)
         else:
             api_metrics.append(metric)
-    return api_metrics, custom_metrics
+    return api_metrics, custom_metrics, ragas_metrics
 
 
 def _aggregate_summary_metrics(
@@ -286,6 +329,13 @@ def _aggregate_summary_metrics(
                     ]
                     == "BASELINE"
                 ).mean()
+            elif isinstance(metric, RagasMetric):
+                summary_metrics[f"{metric.name}/mean"] = metrics_table.loc[
+                    :, f"{metric.name}/{constants.MetricResult.SCORE_KEY}"
+                ].mean()
+                summary_metrics[f"{metric.name}/std"] = metrics_table.loc[
+                    :, f"{metric.name}/{constants.MetricResult.SCORE_KEY}"
+                ].std()
             else:
                 summary_metrics[f"{str(metric)}/mean"] = metrics_table.loc[
                     :, f"{str(metric)}/{constants.MetricResult.SCORE_KEY}"
@@ -498,9 +548,9 @@ def _run_model_inference(
                     )
                 t2 = time.perf_counter()
                 _LOGGER.info(f"Multithreaded Batch Inference took: {t2 - t1} seconds.")
-                evaluation_run_config.metric_column_mapping[
+                evaluation_run_config.metric_column_mapping[response_column_name] = (
                     response_column_name
-                ] = response_column_name
+                )
             else:
                 raise ValueError(
                     "Missing required input `prompt` column to start model inference."
@@ -586,15 +636,15 @@ def _assemble_prompt_for_dataset(
     )
 
     try:
-        evaluation_run_config.dataset[
-            constants.Dataset.PROMPT_COLUMN
-        ] = evaluation_run_config.dataset.apply(
-            lambda row: str(
-                prompt_template.assemble(
-                    **row[list(prompt_template.variables)].astype(str).to_dict(),
-                )
-            ),
-            axis=1,
+        evaluation_run_config.dataset[constants.Dataset.PROMPT_COLUMN] = (
+            evaluation_run_config.dataset.apply(
+                lambda row: str(
+                    prompt_template.assemble(
+                        **row[list(prompt_template.variables)].astype(str).to_dict(),
+                    )
+                ),
+                axis=1,
+            )
         )
         if (
             constants.Dataset.PROMPT_COLUMN
@@ -611,9 +661,9 @@ def _assemble_prompt_for_dataset(
                 " parameter is provided. Please verify that you want to use"
                 " the assembled `prompt` column for evaluation."
             )
-        evaluation_run_config.metric_column_mapping[
+        evaluation_run_config.metric_column_mapping[constants.Dataset.PROMPT_COLUMN] = (
             constants.Dataset.PROMPT_COLUMN
-        ] = constants.Dataset.PROMPT_COLUMN
+        )
     except Exception as e:
         raise ValueError(
             f"Failed to assemble prompt template: {e}. Please make sure all"
@@ -707,6 +757,13 @@ def _parse_metric_results_to_dataframe(
                 metrics_table,
                 constants.MetricResult.SCORE_KEY,
             )
+        elif isinstance(metric, RagasMetric):
+            _set_metric_table(
+                metric.name,
+                metric_results,
+                metrics_table,
+                constants.MetricResult.SCORE_KEY,
+            )
         else:
             _LOGGER.warning(
                 f"Metric name: {str(metric)} is not supported when parsing"
@@ -738,7 +795,7 @@ def _compute_metrics(
             ' google-cloud-aiplatform[evaluation]"'
         )
 
-    api_metrics, custom_metrics = _separate_custom_metrics(
+    api_metrics, custom_metrics, ragas_metrics = _separate_metrics(
         evaluation_run_config.metrics
     )
     row_count = len(evaluation_run_config.dataset)
@@ -792,6 +849,22 @@ def _compute_metrics(
         results_dict[metric] = [
             _instance_evaluation.handle_response(response) for response in responses
         ]
+
+    if ragas_metrics:
+        ragas_eval_dataset = _validate_ragas_metrics_columns(
+            evaluation_run_config=evaluation_run_config
+        )
+        ragas_result = ragas_evaluate(
+            dataset=ragas_eval_dataset,
+            metrics=ragas_metrics,
+        )
+
+        for ragas_metric in ragas_metrics:
+            results_dict[ragas_metric] = [
+                {constants.MetricResult.SCORE_KEY: score}
+                for score in ragas_result[ragas_metric.name]
+            ]
+
     if error_list:
         _LOGGER.warning(
             f"{len(error_list)} errors encountered during evaluation. Continue to"
